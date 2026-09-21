2a07823096c7627ae5128e383b0ec5b17c697660
Kou Watanabe <kouchan.watanabe@icloud.com>
2026-09-21 21:22:51 +0900

    Compact live cards

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { usePathname } from "next/navigation";
-
 export default function PageTransition({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-
-  return (
-    <div key={pathname} className="page-enter">
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
