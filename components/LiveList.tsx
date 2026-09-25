255094db18a10d9610d470eb2dface60c3b792f3
Kou Watanabe <kouchan.watanabe@icloud.com>
2026-09-25 22:50:36 +0900

    Compact live cards

--- components/LiveList.tsx
+++ components/LiveList.tsx
@@ -400,7 +400,7 @@ export default function LiveList({
               <div
                 className="
                   absolute
-                  right-5
+                  right-8
                   top-2
                   z-10
                   sm:right-4
