33aacd6b5524cd32ba7b1f46d704edb0a506f3f9
Kou Watanabe <kouchan.watanabe@icloud.com>
2026-09-27 02:24:04 +0900

    Compact live cards

--- components/LiveCalendar.tsx
+++ components/LiveCalendar.tsx
@@ -1671,6 +1671,7 @@ const handleTouchCancel =
       {/* ================================= */}
 
       <div
+      key={`${calendarYear}-${calendarMonth}`}
         ref={
           calendarContainerRef
         }
